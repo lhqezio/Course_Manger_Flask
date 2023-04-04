9f222e09d91a694e02d1755495a8a8d54be7e3e5
Vasilii Iurev <vasilii.iurev@dawsoncollege.qc.ca>
2023-04-04 14:10:05 -0400

    add course_views blueprint; add courses.html template

---
+++ CourseManager/CoursesDisplay/course_views.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, render_template, redirect, url_for, flash
+import oracledb
+
+bp = Blueprint('course', __name__, url_prefix='/courses')
+
+@bp.route('/')
+def display_courses():
+    try:
+        return render_template("courses.html", courses=get_db().get_courses(),
+                               terms=get_db().get_terms(), domains=get_db().get_domains())
+    except oracledb.Error as e:
+        flash("Something went wrong..")
+        flash("Cannot reach the database")
+        return redirect(url_for("home.index"))
+
+@bp.route('/<course_id>')
+def display_course(course_id):
+    if get_db().get_course(course_id):
+        return render_template("specific_course.html", course=get_db().get_course(course_id))
+    flash(f"{course_id} course not found!")
+    return redirect(url_for("course.display_courses"))
