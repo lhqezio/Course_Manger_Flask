478ea6e6de559de8829212fb10876609cfe0ebec
Anastasia Bondarenko <anastasia.bondarenko@dawsoncollege.qc.ca>
2023-04-04 13:25:41 -0400

    course object and course form

--- CourseManager/course.py
+++ CourseManager/course.py
@@ -1,7 +1,7 @@
-#from flask_wtf import FlaskForm
-# from flask import jsonify
-# from wtforms import StringField
-# from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm
+from flask import jsonify
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired
 
 class Course:
     def __init__(self,courseNumber,courseTitle,theoryHours,labHours,
@@ -31,16 +31,29 @@ class Course:
         self.domainID=domainID
         self.term=term
     def __str__(self):
-        return f''
+        return f'{self.courseNumber} {self.courseTitle}: {self.theoryHours}-{self.labHours}-{self.homeworkHours}'
+    def __repr__(self):
+        return f'{self.courseNumber} {self.courseTitle}: {self.theoryHours}-{self.labHours}-{self.homeworkHours}'
     def __eq__(self, obj):
         if not isinstance(obj,Course):
             raise Exception("Not a course object")
-        if(self.name!=obj.name):
+        if(self.courseNumber!=obj.courseNumber):
             return False
         return True
     def from_json(json):
         if not isinstance(json,dict):
-            raise TypeError("Not an Address")
-        #return Cpourse(json['name'],json['street'],json['city'],json['province'])
+            raise TypeError("Not a course")
+        return Course(json['courseNumber'],json['courseTitle'],json['theoryHours'],json['labHours'],
+                      json['homeworkHours'],json['description'],json['domainID'],json['term'])
     def to_json(self):
-        #return jsonify(self.__dict__)
+        return jsonify(self.__dict__)
+
+class CourseForm(FlaskForm):
+    courseNumber = StringField('course number',validators=[DataRequired()])
+    courseTitle = StringField('course title',validators=[DataRequired()])
+    theoryHours = IntegerField('theory hours',validators=[DataRequired()])
+    labHours = IntegerField('lab hours',validators=[DataRequired()]),
+    homeworkHours = IntegerField('homework hours',validators=[DataRequired()])
+    description = StringField('description',validators=[DataRequired()])
+    domainID = StringField('domain id',validators=[DataRequired()])
+    term = StringField('term',validators=[DataRequired()])
