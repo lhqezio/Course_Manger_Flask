244b7d8592575bca07bfe9f8865f26fa5da4359c
Vasilii Iurev <yuryev.v@Vasiliis-Macbook-Pro.local>
2023-04-11 13:55:16 -0400

    add more views

--- CourseManager/__init__.py
+++ CourseManager/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 import secrets
 from CourseManager.CoursesDisplay.course_views import bp as course_views
 from CourseManager.CoursesDisplay.competency_views import bp as competency_views
+from CourseManager.CoursesDisplay.element_views import bp as element_views
+from CourseManager.CoursesDisplay.home_views import bp as home_views
 from CourseManager.dbmanager import *
 
 def create_app(test_config=None):
@@ -17,6 +19,8 @@ def init_app(app):
     app.teardown_appcontext(cleanup)
     app.register_blueprint(course_views)
     app.register_blueprint(competency_views)
+    app.register_blueprint(home_views)
+    app.register_blueprint(element_views)
     with app.app_context():
         db = get_db()
 
