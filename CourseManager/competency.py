a99bc78ea7edc65caa3bbc38c8718f12d1c91e81
Belaru <nactiabond.ota@gmail.com>
2023-04-11 13:31:13 -0400

    naming convention

--- CourseManager/competency.py
+++ CourseManager/competency.py
@@ -4,23 +4,23 @@ from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired
 
 class Competency:
-    def __init__(self,competencyID,competency,competencyAchievement,competencyType):
-        if not isinstance(competencyID,int):
+    def __init__(self,competency_id,competency,competency_achievement,competency_type):
+        if not isinstance(competency_id,int):
             raise TypeError()
         if not isinstance(competency,str):
             raise TypeError()
-        if not isinstance(competencyAchievement,str):
+        if not isinstance(competency_achievement,str):
             raise TypeError()
-        if not isinstance(competencyType,str):
+        if not isinstance(competency_type,str):
             raise TypeError()
-        self.competencyID=competencyID
+        self.competency_id=competency_id
         self.competency=competency
-        self.competencyAchievement=competencyAchievement
-        self.competencyType=competencyType
+        self.competency_achievement=competency_achievement
+        self.competency_type=competency_type
     def __str__(self):
-        return f'{self.competencyID} {self.competency}: {self.competencyAchievement} type:{self.competencyType}'
+        return f'{self.competency_id} {self.competency}: {self.competency_achievement} type:{self.competency_type}'
     def __repr__(self):
-        return f'{self.competencyID} {self.competency}: {self.competencyAchievement} type:{self.competencyType}'
+        return f'{self.competency_id} {self.competency}: {self.competency_achievement} type:{self.competency_type}'
     def __eq__(self, other):
         if not isinstance(other,Competency):
             raise Exception("Not a competency object")
@@ -28,12 +28,12 @@ class Competency:
     def from_json(json):
         if not isinstance(json,dict):
             raise TypeError("Not a competency")
-        return Competency(json['competencyID'],json['competency'],json['competencyAchievement'],json['labHours'])
+        return Competency(json['competency_id'],json['competency'],json['competency_achievement'],json['competency_type'])
     def to_json(self):
         return jsonify(self.__dict__)
     
 class CompetencyForm(FlaskForm):
-    competencyID = IntegerField('competency id',validators=[DataRequired()])
+    competency_id = IntegerField('competency id',validators=[DataRequired()])
     competency = StringField('competency',validators=[DataRequired()])
-    competencyAchievement = StringField('competency achievement',validators=[DataRequired()])
-    competencyType = StringField('competency type',validators=[DataRequired()])
+    competency_achievement = StringField('competency achievement',validators=[DataRequired()])
+    competency_type = StringField('competency type',validators=[DataRequired()])
