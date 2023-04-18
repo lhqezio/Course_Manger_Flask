3846905f6ea8692cfb978826c45b7444b9736744
Vasilii Iurev <yuryev.v@Vasiliis-Macbook-Pro.local>
2023-04-18 13:45:04 -0400

    add element templates

--- CourseManager/CoursesDisplay/course_views.py
+++ CourseManager/CoursesDisplay/course_views.py
@@ -21,9 +21,6 @@ def display_courses(domain_id):
 @bp.route('/<course_id>')
 def display_course(course_id):
     if get_db().get_course(course_id):
-        course = get_db().get_course(course_id)
-        return render_template("specific_course.html", course=course,
-                               domain=get_db().get_domain(course.domain_id),
-                               term=get_db().get_term(course.term_id))
+        return render_template("specific_course.html", course=get_db().get_course(course_id))
     flash(f"{course_id} course not found!")
     return redirect(url_for("course.display_courses"))
